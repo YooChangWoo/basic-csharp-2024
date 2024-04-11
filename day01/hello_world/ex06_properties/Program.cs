6e4f2a3ffe73c260dd3bcddba0570170c283b8f3
YooChangWoo <dbckddn1201@naver.com>
2024-04-11 17:26:46 +0900

    day 01

---
+++ day01/hello_world/ex06_properties/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ex06_properties
+{
+    class Kiturami
+    {
+        public int temperature; //  온도
+
+        // Rosalyn VS 개발서포터
+
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    temperature = 20; // 10도 이하는 허용안함
+                }
+                else if (value > 70)
+                {
+                    temperature = 50; // 70도 초과는 허용안함
+                else
+                        temperature = value;
+                }
+                temperature = value;
+            }
+        }
+
+        //   public int SetTemperature(int temp)
+        //   { 
+        //       if (temp > 70)
+        //       {
+        //           Console.WriteLine("온도가 너무 높습니다. 50도로 조정합니다.");
+        //           this.temperature = 50;
+        //       }
+        //       else if ( temp < 10 )
+        //       {
+        //           Console.WriteLine("온도가 너무 낮습니다. 20도로 조정합니다.");
+        //           this.temperature = temp;
+        //       }
+        //       else
+        //       {
+        //          this.temperature = temp;
+        //       }
+        //        
+        //   }
+
+        public int GetTemperature() 
+        { 
+            return this.temperature;
+        }
+        public void On()
+        {
+            Console.WriteLine("보일러 On");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("보일러 Off");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("보일러 시작!");
+            Kiturami boiler = new Kiturami();
+            //boiler.temperature = 400; // 막아버림
+            //Console.WriteLine($"보일러 온도는 {boiler.temperature}도"); // 막아버림
+            //boiler.temperature = 400;
+            //boiler.SetTemperature($"보일러 온도는 {boiler.GetTemperature()} 도");
+            boiler.Temperature = 400;
+            Console.WriteLine($"보일러 온도는 {boiler.GetTemperature()}도");
+            boiler.On();
+        }
+    }
+}
