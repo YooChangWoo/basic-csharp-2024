b188a166196945cc52fe539a7fbd8eddce9f794b
YooChangWoo <dbckddn1201@naver.com>
2024-04-19 17:14:46 +0900

    day07

--- README.md
+++ README.md
@@ -329,6 +329,12 @@
 
 
     - 도서관리 앱 with SQL Server(Base) ModernUI(NuGet패키지)
+    ```cs
+    // 값형식 변수에 null값을 넣을 수 있도록 만들어준 기능 Nullable. 변수명 뒤에 ?만 추가할 것
+    int? a = null;
+    double? b = null;
+    float? c = null;
+    ```
 
 ## 8일차
 - 토이 프로젝트
---
+++ day07/cs07_toyproject/NewBookRentalShopApp/FrmMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace NewBookRentalShopApp
+{
+    public partial class FrmMain : MetroForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FrmLogin frm = new FrmLogin();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.TopMost = true;
+            frm.ShowDialog();
+        }
+    }
+}
